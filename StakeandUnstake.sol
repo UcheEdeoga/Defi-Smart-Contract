b8a54e68fb23a29969c9ef7fbba28b1220fb3e47
UcheEdeoga <Uchejohnedeoga@gmail.com>
2021-06-23 23:33:08 -0700

    Update on APY Implementation

--- StakeandUnstake.sol
+++ StakeandUnstake.sol
@@ -46,13 +46,28 @@ contract StakeandUnstake {
      //Issue Tokens to users
       function issueTokens() public {
       require(msg.sender == owner);
+      for (uint i=0; i<stakers.length; i++) {
+            address recipient = stakers[i];
+            uint balance = stakingBalance[msg.sender];
       if (balance < 500) {
          avaToken.transfer(recipient, balance);
+         balance = balance*8/100 * 30; //User gets 8% APY when they stake less than 500 tokens ie when their balance is less than 500
       }
 
-      if (balance >= 500 && <= 2000) {
+      if (balance >= 500 && < 1000) {
          avaToken.transfer(recipient, balance);
+         balance = balance*10/100 * 30; //User gets 10% APY when they stake grater than or equal to 500 tokens i.e their balance is greater than or equal to 500 but less than 1000
       }
+
+      if (balance >=1000 && < 1500 ) {
+      avaToken.transfer(recipient, balance);
+      balance = balance*15/100 * 30; //Users gets 15% APY when they stake greater than or equal to 1000 tokens i.e when their balance is 1000 tokens or more but less than 1500
+   }
+
+   if (balance >= 1500) {
+   avaToken.transfer(recipient, balance);
+   balance = balance*25/100 * 30; //User gets 25% APY When they stake greater or equal to 1500 tokens i.e when their balance is greater or equal to 1500
+      }
+
    }
 }
-
